da404e51d6c22c1dd456f29694f134e80e3fd08f
QKevCheng <qcheng16@gmail.com>
2018-04-17 14:41:30 -0400

    first visualization

--- src/App.vue
+++ src/App.vue
@@ -32,17 +32,18 @@
         </ul>
       </div> -->
 
-      <div v-if="top_tracks">
+      <avgfeat v-if="time_selected" :user_selected="time_selected" :audioFeatures_short="top_tracks_short_features" :audioFeatures_medium="top_tracks_medium_features" :audioFeatures_long="top_tracks_long_features"></avgfeat>
+
+      <div v-if="time_selected">
         <h3>Top 10 Tracks</h3>
         <ul>
-          <li class="tracks" v-for="track in top_tracks.items">
+          <li class="tracks" v-for="track in top_tracks_display.items">
             <p> {{ track.name }} </p> <br>
             <img class="track_image" :src="track.album.images[1].url" />
           </li>
         </ul>
       </div>
 
-      <avgfeat v-if="top_tracks_features" :audioFeatures="top_tracks_features"></avgfeat>
   </div>
 </template>
 
@@ -56,15 +57,27 @@ export default {
     return {
         profile: null,
         top_artists: null,
-        top_tracks: null,
-        top_tracks_features: null,
+        top_tracks_display: null,
+        top_tracks_short: null,
+        top_tracks_medium: null,
+        top_tracks_long: null,
+        top_tracks_short_features: null,
+        top_tracks_medium_features: null,
+        top_tracks_long_features: null,
         time_selected: null,
         time_options: [
-            { value: 'short_term', text: "short term" },
-            { value: 'medium_term', text: "medium term" },
-            { value: 'long_term', text: "long term" }
+            { value: 'short', text: "short term" },
+            { value: 'medium', text: "medium term" },
+            { value: 'long', text: "long term" }
         ]
     }
+  },
+  watch: {
+      time_selected: function(val) {
+          if (val === 'short') this.top_tracks_display = this.top_tracks_short;
+          else if (val === 'medium') this.top_tracks_display = this.top_tracks_medium;
+          else this.top_tracks_display = this.top_tracks_long;
+      },
   },
   methods: {
       sign_in() {
@@ -74,7 +87,7 @@ export default {
       async access_api() {
           // parse the access token from the window
           let parsed = queryString.parse(window.location.search);
-          let accessToken = parsed.access_token;
+          var accessToken = parsed.access_token;
 
           // access Spotify api with access token
           // personal info
@@ -89,53 +102,111 @@ export default {
               console.log("Profile", data);
               this.profile = data;
           });
+
           // top artists
-          await $.ajax({
-              url: 'https://api.spotify.com/v1/me/top/artists?time_range='+ this.time_selected +'&limit=10&offset=0',
+        //   await fetch('https://api.spotify.com/v1/me/top/artists?time_range='+ this.time_selected +'&limit=10&offset=0' , {
+        //       method: 'GET',
+        //       headers: {
+        //           'Authorization': 'Bearer ' + accessToken
+        //       },
+        //   }).then(raw_data => {
+        //       return raw_data.json();
+        //   }).then(data => {
+        //       console.log("Top Track Short", data);
+        //       this.top_artists = data;
+        //   });
+
+          // top tracks in short term
+          await fetch('https://api.spotify.com/v1/me/top/tracks?time_range=short_term&limit=10&offset=0' , {
+              method: 'GET',
               headers: {
                   'Authorization': 'Bearer ' + accessToken
               },
-              success: data => {
-                  console.log("Top Artists", data);
-                  this.top_artists = data;
-              }
+          }).then(raw_data => {
+              return raw_data.json();
+          }).then(data => {
+              console.log("Top Track Short", data);
+              this.top_tracks_short = data;
           });
-          // top tracks
-          await $.ajax({
-              url: 'https://api.spotify.com/v1/me/top/tracks?time_range=' + 'medium_term' + '&limit=10&offset=0',
+
+          // top tracks in medium term
+          await fetch('https://api.spotify.com/v1/me/top/tracks?time_range=medium_term&limit=10&offset=0' , {
+              method: 'GET',
               headers: {
                   'Authorization': 'Bearer ' + accessToken
               },
-              success: data => {
-                  console.log("Top Tracks", data);
-                  this.top_tracks = data;
-              }
+          }).then(raw_data => {
+              return raw_data.json();
+          }).then(data => {
+              console.log("Top Track Mid", data);
+              this.top_tracks_medium = data;
           });
 
-         let track_ids = "";
+          // top tracks in long term
+          await fetch('https://api.spotify.com/v1/me/top/tracks?time_range=long_term&limit=10&offset=0' , {
+              method: 'GET',
+              headers: {
+                  'Authorization': 'Bearer ' + accessToken
+              },
+          }).then(raw_data => {
+              return raw_data.json();
+          }).then(data => {
+              console.log("Top Track Long", data);
+              this.top_tracks_long = data;
+          });
 
-         // save the ids of top tracks into a string
-         this.top_tracks.items.forEach(track => {
-             track_ids += "2C" + track.id + "%";
-         });
 
-         //
-         track_ids = track_ids.substring(2, track_ids.length-1);
-
-         // get the audio features of top tracks
-         await fetch('https://api.spotify.com/v1/audio-features?ids=' + track_ids, {
-             method: 'GET',
-             mode: 'cors',
-             headers: {
-                 'Authorization': 'Bearer ' + accessToken
-             },
-         }).then(raw_data => {
-             return raw_data.json();
-         }).then(data => {
-             console.log("Features", data);
-             this.top_tracks_features = data;
-         });
+          this.get_features('short');
+          this.get_features('mid');
+          this.get_features('long');
       },
+      async get_features(term) {
+          // parse the access token from the window
+          let parsed = queryString.parse(window.location.search);
+          var accessToken = parsed.access_token;
+
+          let tracks_data = null;
+          let features_data = null
+          if (term === "short") {
+              tracks_data = this.top_tracks_short;
+          } else if (term === "mid") {
+              tracks_data = this.top_tracks_medium;
+          } else {
+              tracks_data = this.top_tracks_long;
+          }
+
+          let track_ids = "";
+
+          // save the ids of top tracks into a string
+          tracks_data.items.forEach(track => {
+              track_ids += "2C" + track.id + "%";
+          });
+
+          //
+          track_ids = track_ids.substring(2, track_ids.length-1);
+
+          // get the audio features of top tracks
+          await fetch('https://api.spotify.com/v1/audio-features?ids=' + track_ids, {
+              method: 'GET',
+              mode: 'cors',
+              headers: {
+                  'Authorization': 'Bearer ' + accessToken
+              },
+          }).then(raw_data => {
+              return raw_data.json();
+          }).then(data => {
+              if (term === "short") {
+                  this.top_tracks_short_features = data;
+                  console.log(this.top_tracks_short_features);
+              } else if (term === "mid") {
+                  this.top_tracks_medium_features = data;
+                  console.log(this.top_tracks_medium_features);
+              } else {
+                  this.top_tracks_long_features = data;
+                  console.log(this.top_tracks_long_features);
+              }
+          });
+      }
   },
   mounted() {
   }
--- src/component/avg_audio_feature.vue
+++ src/component/avg_audio_feature.vue
@@ -1,11 +1,11 @@
 <template>
     <div>
         <h3>This is what your top tracks look like</h3>
-         <button type="button" class="btn btn-danger" @click="analyze_features(), draw_chart()">Analyze</button>
-        <div id="chart">
-            <ul>
-                <svg></svg>
-                <li v-for="feat in avg_features">
+        <div id="chart"></div>
+         <!-- <button type="button" class="btn btn-danger" @click="analyze_features(), draw_graph()">Analyze</button> -->
+         <div id="wrapper">
+            <ul v-if="avg_features != null">
+                <li v-for="feat in avg_features[dataset_index]">
                     {{ feat.type }}: {{ feat.point }}
                 </li>
             </ul>
@@ -17,17 +17,43 @@
 import * as d3 from "d3";
 
 export default {
-    props: ['audioFeatures'],
+    props: ['user_selected', 'audioFeatures_short', 'audioFeatures_medium', 'audioFeatures_long'],
     data() {
         return {
-            features: this.audioFeatures,
-            avg_features: null
+            features_short: this.audioFeatures_short,
+            features_medium: this.audioFeatures_medium,
+            features_long: this.audioFeatures_long,
+            avg_features: null,
+            graph_height: null,
+            dataset_index: 0
         }
     },
-    computed: {
+    mounted() {
+        this.set_dataset_index();
+        this.analyze_features();
+        this.pack_features();
+        this.draw_graph();
+    },
+    watch: {
+        user_selected: function() {
+            // once time period has been changed, change the graph accordingly
+            this.set_dataset_index();
+            // this.graph_transition();
+            this.draw_graph();
+        }
     },
     methods: {
-        analyze_features() {
+        set_dataset_index() {
+            if(this.user_selected === "short") this.dataset_index = 0;
+            else if(this.user_selected === "medium") this.dataset_index = 1;
+            else this.dataset_index = 2;
+        },
+        analyze_features(input) {
+            let features = null;
+            if(input === "short") features = this.features_short;
+            else if(input === "medium") features = this.features_medium;
+            else features = this.features_long;
+
             let counter = 0;
 
             let avg_feat = [
@@ -41,81 +67,112 @@ export default {
                 // {point: 0, type: tempo }
             ]
 
-            this.features.audio_features.forEach(track => {
+            features.audio_features.forEach(track => {
                 // accumulate each feature of all tracks
                 avg_feat.forEach(feat => {
-                    feat.point = track[feat.type];
+                    feat.point += track[feat.type];
                 })
                 counter ++;
             })
 
             // get the average numbers
             avg_feat.forEach(feat => {
-                feat.point = feat.point/counter;
+                feat.point = (feat.point*10/counter).toFixed(1);
             })
 
-            this.avg_features = avg_feat;
+            return avg_feat;
         },
-        draw_chart() {
-            var temperatures = [
-  {point: 0.6, type: 'January'},
-  {point: 0.1, type: 'February'},
-  {point: 0.3, type: 'March'},
-  {point: 0.8, type: 'April'}
-];
-            var types = this.avg_features.map(feat => {
-                console.log(feat.type)
+        pack_features() {
+            let short = this.analyze_features("short");
+            let medium = this.analyze_features("medium");
+            let long = this.analyze_features("long");
+            this.avg_features = [short, medium, long];
+        },
+        draw_graph() {
+            d3.select("#chart > *").remove();
+            // list of features
+            var types = this.avg_features[0].map(feat => {
                 return feat.type
             });
-
-            var fullWidth = 700;
-            var fullHeight = 200;
-            var margin = {top: 20, right: 20, bottom: 30, left: 40};
+            // width and height of svg element
+            var fullWidth = 500;
+            var fullHeight = 350;
+            // width and height of the bar chart
+            var margin = {top: 5, right: 5, bottom: 50, left: 50};
             var width = fullWidth - margin.left - margin.right;
             var height = fullHeight - margin.top - margin.bottom;
+            this.graph_height = height;
+            // var color = d3.scale.category20();
 
-            var svg = d3.select('svg').append('svg')
-                        .append('g').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+            var svg = d3.select('#chart').append('svg')
+            .attr('width', fullWidth)
+            .attr('height', fullHeight)
+            // g is the where the chart will be drawn
+            .append('g')
+            // leave room for top and left of the graph
+            .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
+            // x value are the different features of audio
             var feature_scale = d3.scaleBand()
                                 .domain(types)
                                 .range([0, width])
-                                .paddingInner(0.1);
+                                .paddingInner(0.3);
 
+            // the width of each bar
             var bandwidth = feature_scale.bandwidth();
 
-            var max_point = 1;
+            // y value indicating the points on different features
+            var max_point = 10;
             var point_scale = d3.scaleLinear()
                                 .domain([0, max_point])
                                 .range([height, 0])
+                                // rounding extreme values
                                 .nice();
 
+            // set x and y axis
             var xAxis = d3.axisBottom(feature_scale);
             var yAxis = d3.axisLeft(point_scale);
 
+            // draw x and y axis
             var xAxisEle = svg.append('g')
                 .classed('x axis', true)
                 .attr('transform', 'translate(0,' + height + ')')
                 .call(xAxis);
 
             var yAxisEle = svg.append('g')
-                                .classed('y axis', true)
-                                .call(yAxis);
+                .classed('y axis', true)
+                .call(yAxis);
+
+            // label the axis
+            var yText = xAxisEle.append('text')
+                        .attr('transform', 'translate('+ width/2 +', 40)')
+                        .style('text-anchor', 'middle')
+                        .style('fill', 'black')
+                        .attr('dx', '-10mm')
+                        .style('font-size', 14)
+                        .text('Feature');
 
             var yText = yAxisEle.append('text')
                       .attr('transform', 'rotate(-90)translate(-' + height/2 + ',0)')
                       .style('text-anchor', 'middle')
                       .style('fill', 'black')
-                      .attr('dy', '-2.5em')
+                      .attr('dy', '-6mm')
                       .style('font-size', 14)
                       .text('Point');
 
             var barHolder = svg.append('g')
-                                .classed('bar-holder', true);
+                    .classed('bar-holder', true);
 
-                                var bars = barHolder.selectAll('rect.bar')
-                        .data(temperatures)
-                      .enter().append('rect')
+            function color_scheme(i) {
+                let max_count = 0;
+                types.forEach(t => max_count++)
+                return (d3.interpolateWarm(i/max_count));
+            }
+            // draw the bars
+            var bars = barHolder.selectAll('rect.bar')
+                        .data(this.avg_features[this.dataset_index])
+                        .enter()
+                        .append('rect')
                         .classed('bar', true)
                         .attr('x', function(d, i) {
                           return feature_scale(d.type);
@@ -127,9 +184,55 @@ export default {
                         .attr('height', function(d) {
                           // the bar's height should align it with the base of the chart (y=0)
                           return height - point_scale(d.point);
-                        });
+                        })
+                        .attr('fill', function(d, i){return color_scheme(i)})
+                        .append("text")
+                        .text(function(d){
+                            console.log(d.point)
+                            return d.point})
+                        .attr("text-anchor", "middle")
+                        .attr('x', function(d, i) {
+                          return feature_scale(d.type);
+                        })
+                        .attr('y', function(d) {
+                          return point_scale(d.point)-20;
+                        })
+                        .attr("font-family", "sans-serif")
+                        .attr("font-size", "11px")
+                        .attr("fill", "red");
 
-        }
+
+
+        },
+        graph_transition() {
+            var point_scale = d3.scaleLinear()
+                                .domain([0, 10])
+                                .range([this.graph_height, 0])
+                                // rounding extreme values
+                                .nice();
+
+            d3.selectAll(".bar")
+                .transition()
+                // .attr("height", function(d) {
+                //     return this.graph_height - point_scale(d.point);
+                // })
+                // .attr("x", function(d) {
+                //     console.log(d);
+                // })
+                .attr("y", function(d) {
+                    return point_scale(d.point);
+                })
+                // .ease("linear")
+                // .select("title")
+                // .text(function(d) {
+                //
+                // });
+        },
+
     },
 }
 </script>
+
+<style>
+
+</style>
