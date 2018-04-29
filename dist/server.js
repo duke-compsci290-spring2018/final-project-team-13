4fdbd9d8f6b252c2eba95e2a96317a132b49f1a5
ElbertW <elbertwang187@gmail.com>
2018-04-29 10:22:28 -0400

    Another try

--- dist/server.js
+++ dist/server.js
@@ -3,6 +3,6 @@ var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
 app.use(serveStatic(__dirname));
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port);
 console.log('server started '+ port);
